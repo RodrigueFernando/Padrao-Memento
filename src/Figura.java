444e07447b7cceae1b82a991a6ee3889bca76be8
Fernnado <Ferdinhokirei666@gmail.com>
2025-11-16 16:29:43 -0300

    Criação da classe Figura, parâmetros e construtor

---
+++ src/Figura.java
@@ -0,0 +1,15 @@
+public class Figura { // Classe Originator
+
+    private final String cor;
+    private final int posicaoX;
+    private final int posicaoY;
+
+    public Figura(String cor, int posicaoX, int posicaoY) {
+        this.cor = cor;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+    }
+
+
+
+}
