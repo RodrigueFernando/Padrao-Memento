31f248ea00ff958e3c2769749a0eb22acf6d3d7b
Fernnado <Ferdinhokirei666@gmail.com>
2025-11-16 16:37:24 -0300

    Classe Figura, implementando o método mover e mudarCor

--- src/Figura.java
+++ src/Figura.java
@@ -1,13 +1,21 @@
 public class Figura { // Classe Originator
 
-    private final String cor;
-    private final int posicaoX;
-    private final int posicaoY;
+    private  String cor;
+    private  int posicaoX;
+    private  int posicaoY;
 
     public Figura(String cor, int posicaoX, int posicaoY) {
         this.cor = cor;
         this.posicaoX = posicaoX;
         this.posicaoY = posicaoY;
+    }
+
+    public void mover(int novaPosicaoX, int novaPosicaoY) {
+        this.posicaoX = novaPosicaoX;
+        this.posicaoY = novaPosicaoY;
+    }
+    public void mudarCor(String novaCor) {
+        this.cor = novaCor;
     }
 
 
